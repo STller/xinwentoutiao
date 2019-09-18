b7ff1fc89c0c2f314e6345dd0d7394fecbeccf9c
温翔宇 <fishfly516@gmail.com>
2019-09-18 09:17:56 +0800

    添加了element-ui

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Slot from './views/Slot.vue'
+import ScopeSlot from './views/ScopeSlot.vue'
 
 Vue.use(Router)
 
@@ -18,6 +20,12 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }, {
+      path: '/slot',
+      component: Slot
+    }, {
+      path: '/scopeslot',
+      component: ScopeSlot
     }
   ]
 })
