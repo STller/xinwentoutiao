94e63b8a0b06e4a56ed8218d702d44c33e2ee77a
温翔宇 <fishfly516@gmail.com>
2019-09-18 15:04:03 +0800

    axios全局使用

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // 全局样式的设置 比如margin padding
 import './styles/index.less'
+import axios from 'axios'
 Vue.config.productionTip = false
 Vue.use(ElementUI) // vue.use 到底干了什么
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$axios = axios // 将axios给所有vue组件实例
 new Vue({
   router,
   render: h => h(App)
