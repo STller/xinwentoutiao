23ad5cc9ea0a07b0880711cbfc97e11fe74cba86
温翔宇 <fishfly516@gmail.com>
2019-09-20 21:45:58 +0800

    用户信息获取

--- src/components/Home/layout-header.vue
+++ src/components/Home/layout-header.vue
@@ -1,17 +1,17 @@
 <template>
     <el-row type="flex" align="middle" justify="space-between">
-        <el-col :span="21">
+        <el-col :span="20">
             <i class="el-icon-s-unfold"></i>
             <span>江苏传智播客教育科技股份有限公司</span>
         </el-col>
-        <el-col :span="3">
+        <el-col :span="4">
             <!-- 用户头像 -->
-            <img style="vertical-align:middle" src="../../assets/img/avatar.jpg" alt="">
+            <img style="vertical-align:middle;width:50px" :src="userInfo.photo" alt="">
             <!-- 下拉菜单组件 -->
             <el-dropdown trigger="click">
                 <!-- 匿名插槽  -->
                 <span >
-                83期同学
+                {{userInfo.name}}
                 <i class="el-icon-arrow-down el-icon--right"></i>
                 </span>
                 <!-- 具名插槽 dropdown -->
@@ -27,7 +27,29 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      userInfo: {}, // 个人信息对象
+      defaultImg: require('../../assets/img/avatar.jpg') // 转成base64
+    }
+  },
+  methods: {
+    // 获取用户个人资料
+    getUserInfo () {
+      let token = window.localStorage.getItem('user-token') // 从前端缓存中获取token
+      this.$axios({
+        url: '/user/profile',
+        headers: { 'Authorization': `Bearer ${token}` } // 请求参数
+      }).then(result => {
+        this.userInfo = result.data.data // 接收数据对象
+        console.log(this.userInfo)
+      })
+    }
+  },
+  // 钩子函数
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
