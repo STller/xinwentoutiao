3891f72b5cde7ef42d0deccf5de5a83e36b7850d
温翔宇 <fishfly516@gmail.com>
2019-09-20 21:59:45 +0800

    点击头部退出登录完成

--- src/components/Home/layout-header.vue
+++ src/components/Home/layout-header.vue
@@ -6,9 +6,10 @@
         </el-col>
         <el-col :span="4">
             <!-- 用户头像 -->
-            <img style="vertical-align:middle;width:50px" :src="userInfo.photo" alt="">
+            <img style="vertical-align:middle;width:50px;border-radius:50%" :src="userInfo.photo" alt="">
             <!-- 下拉菜单组件 -->
-            <el-dropdown trigger="click">
+            <!-- 注册command事件 -->
+            <el-dropdown @command="commonClick" trigger="click">
                 <!-- 匿名插槽  -->
                 <span >
                 {{userInfo.name}}
@@ -16,9 +17,10 @@
                 </span>
                 <!-- 具名插槽 dropdown -->
                 <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item >个人信息</el-dropdown-item>
-                    <el-dropdown-item >git地址</el-dropdown-item>
-                    <el-dropdown-item >退出</el-dropdown-item>
+                    <!-- command属性会在触发点击时 携带给command事件对应的方法 -->
+                    <el-dropdown-item command="account">个人信息</el-dropdown-item>
+                    <el-dropdown-item command="git">git地址</el-dropdown-item>
+                    <el-dropdown-item command="lgout">退出</el-dropdown-item>
                 </el-dropdown-menu>
             </el-dropdown>
         </el-col>
@@ -44,6 +46,16 @@ export default {
         this.userInfo = result.data.data // 接收数据对象
         console.log(this.userInfo)
       })
+    },
+    commonClick (key) { // key是command属性携带回来的
+      if (key === 'account') {
+      } else if (key === 'git') {
+        //   去项目的git地址
+        window.location.href('https://github.com/STller/83heimatoutiao')
+      } else if (key === 'lgout') {
+        window.localStorage.clear()
+        this.$router.push('/login')
+      }
     }
   },
   // 钩子函数
