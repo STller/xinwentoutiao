7ebaef05251500855d97d6f2bbbcad568a67ced2
温翔宇 <fishfly516@gmail.com>
2019-09-23 14:36:24 +0800

    写了获取文章列表方法

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -3,15 +3,36 @@
     <!-- 内容列表 -->
         <bread-crumb slot="header">
             <template slot="title">
-                文章内容
+                内容列表
             </template>
         </bread-crumb>
+        <!-- 表单 -->
+        <el-form style="magin-left:50px">
+            <el-form-item label="文章状态"></el-form-item>
+            <el-form-item label="频道列表"></el-form-item>
+            <el-form-item label="时间选择"></el-form-item>
+        </el-form>
     </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    //   获取文章列表
+    getArticles () {
+      this.$axios({
+        url: '/articles',
+        method: 'get'
+      }).then((result) => {
+        this.list = result.data.data.results
+      })
+    }
+  }
 }
 </script>
 
