69a654b35941a5b93b15b585f725f93215594593
温翔宇 <fishfly516@gmail.com>
2019-09-25 21:47:00 +0800

    完成用户数据修改与保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -4,24 +4,31 @@
       <template slot="title">账户信息</template>
     </bread-crumb>
     <!-- 表单账户信息 -->
-    <el-form label-width="100px" style="margin-left:20px">
-        <el-form-item label='用户名'>
-            <el-input v-model="formData.name" style="width:300px"></el-input>
-        </el-form-item>
-        <el-form-item label='简介'>
-            <el-input v-model="formData.intro" style="width:300px"></el-input>
-        </el-form-item>
-        <el-form-item label='邮箱'>
-            <el-input v-model="formData.email" style="width:300px"></el-input>
-        </el-form-item>
-        <el-form-item label='手机号'>
-            <el-input disabled style="width:300px"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary">保存信息</el-button>
-        </el-form-item>
+    <!-- ref之前不要加: -->
+    <el-form
+      :model="formData"
+      :rules="accountRules"
+      ref="accountForm"
+      label-width="100px"
+      style="margin-left:20px"
+    >
+      <el-form-item prop="name" label="用户名">
+        <el-input v-model="formData.name" style="width:300px"></el-input>
+      </el-form-item>
+      <el-form-item label="简介">
+        <el-input v-model="formData.intro" style="width:300px"></el-input>
+      </el-form-item>
+      <el-form-item prop="email" label="邮箱">
+        <el-input v-model="formData.email" style="width:300px"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号">
+        <el-input disabled style="width:300px"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="saveUser" type="primary">保存信息</el-button>
+      </el-form-item>
     </el-form>
-    <img class="head-img" :src="formData.photo?formData.photo:''" alt="">
+    <img class="head-img" :src="formData.photo?formData.photo:''" alt />
   </el-card>
 </template>
 
@@ -29,20 +36,38 @@
 export default {
   data () {
     return {
-      formData: {
-        name: '',
-        intro: '',
-        email: '',
-        photo: ''
-      },
-      defaultImg: require('../../assets/img/404.png')
+      formData: {},
+      defaultImg: require('../../assets/img/404.png'),
+      accountRules: {
+        name: [
+          { required: true, message: '用户名不能为空' },
+          { min: 1, max: 7, message: '用户名请控制在1～7字符之间' }
+        ],
+        email: [{ required: true, message: '邮箱不能为空' }]
+      }
     }
   },
   methods: {
+    // 保存用户个人信息
+    saveUser () {
+      this.$refs.accountForm.validate((isOk) => {
+        if (isOk) {
+          // 调用修改资料接口
+          this.$axios({
+            url: '/user/profile',
+            method: 'patch',
+            data: this.formData
+          }).then(() => {
+            // 若成功 提示消息
+            this.$message({ message: '保存成功', type: 'success' })
+          })
+        }
+      })
+    },
     getUserInfo () {
       this.$axios({
         url: '/user/profile'
-      }).then((results) => {
+      }).then(results => {
         this.formData = results.data.data
       })
     }
@@ -54,12 +79,12 @@ export default {
 </script>
 
 <style lang='less' scoped>
-.head-img{
-    position: absolute;
-    left: 700px;
-    top: 150px;
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
+.head-img {
+  position: absolute;
+  left: 700px;
+  top: 150px;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
 }
 </style>
