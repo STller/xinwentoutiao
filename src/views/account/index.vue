496e15939826f68b87d8a402134a85c28a5d69f7
温翔宇 <fishfly516@gmail.com>
2019-09-25 21:08:54 +0800

    完成数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,27 +6,51 @@
     <!-- 表单账户信息 -->
     <el-form label-width="100px" style="margin-left:20px">
         <el-form-item label='用户名'>
-            <el-input style="width:300px"></el-input>
+            <el-input v-model="formData.name" style="width:300px"></el-input>
         </el-form-item>
         <el-form-item label='简介'>
-            <el-input style="width:300px"></el-input>
+            <el-input v-model="formData.intro" style="width:300px"></el-input>
         </el-form-item>
         <el-form-item label='邮箱'>
-            <el-input style="width:300px"></el-input>
+            <el-input v-model="formData.email" style="width:300px"></el-input>
         </el-form-item>
         <el-form-item label='手机号'>
-            <el-input style="width:300px"></el-input>
+            <el-input disabled style="width:300px"></el-input>
         </el-form-item>
         <el-form-item>
             <el-button type="primary">保存信息</el-button>
         </el-form-item>
     </el-form>
-    <img class="head-img" src="../../assets/img/404.png" alt="">
+    <img class="head-img" :src="formData.photo?formData.photo:''" alt="">
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      formData: {
+        name: '',
+        intro: '',
+        email: '',
+        photo: ''
+      },
+      defaultImg: require('../../assets/img/404.png')
+    }
+  },
+  methods: {
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then((results) => {
+        this.formData = results.data.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
+}
 </script>
 
 <style lang='less' scoped>
